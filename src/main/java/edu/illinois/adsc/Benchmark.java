ff84be10eed374804e53a84f510d376437752667
Li Wang <wangli1426@gmail.com>
2015-09-04 15:26:26 +0800

    a lot of enhancements covered in this commit.

--- src/main/java/edu/illinois/adsc/Benchmark.java
+++ src/main/java/edu/illinois/adsc/Benchmark.java
@@ -9,23 +9,26 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.util.Vector;
+
 /**
  * Hello world!
- *
  */
-public class Benchmark
-{
-    private final static int TIMEOUT = 1000; // MS
-    private final static int PRODUCER_NUM = 4;
+public class Benchmark {
+    @Option(name = "--runs", aliases = {"-r"}, usage = "set number of runs")
+    private int _runs = 5; // MS
 
-    @Option(name="--time", aliases = {"-h"}, usage = "set evaluation time")
-    private int _executeTime;
+
+    @Option(name = "--time", aliases = {"-t"}, usage = "set report cycles in seconds")
+    private int _reportCycles = 5;
 
     @Option(name = "--help", aliases = {"-h"}, usage = "help")
     private boolean _help;
 
-    public void testMain( String[] args )
-    {
+    @Option(name = "--num-producer", aliases = {"-p"}, usage = "set producer number ")
+    private int _producer_num = 3;
+
+    public void testMain(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
         parser.setUsageWidth(80);
         try {
@@ -42,20 +45,24 @@ public class Benchmark
 
         Handle handle = new Handle();
 
-        DisruptorQueue queue = createQueue("MyQueue",TIMEOUT);
+        Monitor monitor =  handle;
+
+        DisruptorQueue queue = createQueue("MyQueue", 16);
 
         Consumer consumer = new Consumer(queue, handle);
 
         Producer producer = new Producer(queue);
 
-
+        Reporter reporter = new Reporter(monitor, _reportCycles);
 
         try {
-            Report report = execute(producer, consumer, handle ,_executeTime);
-            report.print();
+           execute(producer, consumer, reporter, _reportCycles);
+            reporter.printSummary();
+            System.out.println("------------------------------------------------------");
+            System.out.println("Test finishes, you can kill the program if it does not terminate.");
 
-        }
-        catch (InterruptedException e){
+
+        } catch (InterruptedException e) {
 
         }
     }
@@ -63,6 +70,7 @@ public class Benchmark
     public static void main(String[] args) throws Exception {
         new Benchmark().testMain(args);
     }
+
     private static class Consumer implements Runnable {
         public EventHandler handler;
         private DisruptorQueue queue;
@@ -74,115 +82,190 @@ public class Benchmark
 
         public void run() {
             queue.consumerStarted();
-            try {
-                while(true) {
-                    queue.consumeBatchWhenAvailable(handler);
+            while (true) {
+                try {
+//                    queue.consumeBatchWhenAvailable(handler);
+                    queue.consumeBatch(handler);
+                } catch (RuntimeException e) {
+                    //break
                 }
-            }catch(RuntimeException e) {
-                //break
             }
         }
     }
 
     private static class Producer implements Runnable {
         private DisruptorQueue queue;
+        private int count;
 
         Producer(DisruptorQueue queue) {
             this.queue = queue;
+            count = 0;
         }
 
         public void run() {
-            try {
-                while (true) {
-                    queue.publish(System.currentTimeMillis(), false);
+            while (true) {
+                try {
+                    //queue.publish(0,false);
+//                    System.out.println("Before publish!");
+                    queue.publish(System.currentTimeMillis(), true);
+//                    System.out.println("Publish " + count++);
+                } catch (InsufficientCapacityException e) {
+                    System.err.println(e.getMessage());
+                    return;
                 }
-            } catch (InsufficientCapacityException e) {
-                return;
             }
         }
     }
 
-    interface Reporter {
-        public double processLatency();
+    interface Monitor {
+        public Double processLatency();
         public double processThroughput();
+        public void reset();
     }
 
-    private static class Handle implements EventHandler<Object>, Reporter {
+    private static class Handle implements EventHandler<Object>, Monitor {
         private long totalTicks;
         private long count;
         private boolean started;
         private long startTime;
-        public Handle () {
+
+        public Handle() {
             totalTicks = 0;
             count = 0;
             started = false;
         }
 
         public void onEvent(Object obj, long sequence, boolean endOfBatch) {
-            if(!started) {
+            if (!started) {
                 started = true;
                 startTime = System.currentTimeMillis();
+                count = 0;
+                totalTicks = 0;
             }
-            totalTicks += System.currentTimeMillis() - (Long)obj;
-            count ++;
+//            totalTicks += (Long)obj;
+            totalTicks += System.currentTimeMillis() - (Long) obj;
+            count++;
         }
 
-        public double processLatency() {
-            return (float)totalTicks / count;
+        public Double processLatency() {
+            return (double) totalTicks / count;
         }
 
         public double processThroughput() {
-            return count / (System.currentTimeMillis() - startTime) * 1000;
+            return ((double) count / (System.currentTimeMillis() - startTime)) * 1000;
         }
+
+        public void reset() {
+            started = false;
+        }
+
     }
 
-    static private Report execute(Runnable producer, Runnable consumer, Reporter reporter,int executeSeconds)
+    private void execute(Runnable producer, Runnable consumer, Runnable reporter, int executeSeconds)
             throws InterruptedException {
 
-        Thread[] producerThreads = new Thread[PRODUCER_NUM];
-        for (int i = 0; i < PRODUCER_NUM; i++) {
+        Thread[] producerThreads = new Thread[_producer_num];
+        for (int i = 0; i < _producer_num; i++) {
             producerThreads[i] = new Thread(producer);
             producerThreads[i].start();
         }
 
         Thread consumerThread = new Thread(consumer);
         consumerThread.start();
-        Thread.sleep(executeSeconds);
-        for (int i = 0; i < PRODUCER_NUM; i++) {
-            producerThreads[i].interrupt();
-        }
-        consumerThread.interrupt();
 
-        for (int i = 0; i < PRODUCER_NUM; i++) {
-            producerThreads[i].join(TIMEOUT);
-            if (producerThreads[i].isAlive()) {
-                System.out.print("producer " + i + " is still alive" + producerThreads[i].isAlive());
-                assert (false);
-            }
-        }
-        consumerThread.join(TIMEOUT);
-        if (consumerThread.isAlive()) {
-            System.out.print("consumer is still alive" + consumerThread.isAlive());
-            assert (false);
-        }
+        Thread reporterThread = new Thread(reporter);
+        reporterThread.start();
 
-        Report ret = new Report();
-        ret.averageDelay = reporter.processLatency();
-        ret.throughput = reporter.processThroughput();
 
-        return ret;
+        Thread.sleep((long)((double)_runs + 1.5) * _reportCycles * 1000);
+        reporterThread.interrupt();
+        System.out.println("------------------------------------------------------");
+
+
+
+        return;
+
+
 
     }
+
     private static DisruptorQueue createQueue(String name, int queueSize) {
         return new DisruptorQueue(name, new MultiThreadedClaimStrategy(
-                queueSize), new BlockingWaitStrategy(), 10L);
+                queueSize), new BlockingWaitStrategy(), 16L);
     }
 
-    private static class Report {
-        public double averageDelay;
-        public double throughput;
-        public void print() {
-            System.out.println("Execution Delay: " + averageDelay + "\tThroughput:" + throughput);
+    private static class Reporter implements Runnable  {
+
+        private int reportCycles;
+        private Monitor monitor;
+
+        private Vector<Double> latencies;
+        private Vector<Double> throughputs;
+
+        public Reporter(Monitor monitor, int reportCycles) {
+            this.reportCycles = reportCycles;
+            this.monitor = monitor;
+            latencies = new Vector<Double>();
+            throughputs = new Vector<Double>();
+        }
+
+        private void print() {
+//            System.out.println(monitor.processLatency() + "ms\t\t" + monitor.processThroughput());
+            double latency = monitor.processLatency();
+            double throughput = monitor.processThroughput();
+            System.out.format("%f\t\t%f\n", latency, throughput);
+            latencies.add(latency);
+            throughputs.add(throughput);
+        }
+
+        private void printHeader() {
+            System.out.println("Execution Delay (ms)\tThroughput (tuples/s)");
+        }
+
+        public void printSummary() {
+            System.out.println("Summary:");
+            System.out.println("\t\tMIN\t\tMAX\t\tAVG");
+            System.out.format("Latency   \t%f\t%f\t%f\n", getMin(latencies), getMin(latencies), getAvg(latencies) );
+            System.out.format("Throughput\t%6.2f\t%6.2f\t%6.2f\n", getMin(throughputs), getMin(throughputs), getAvg(throughputs) );
+        }
+
+        private double getMin(Vector<Double> inputs){
+            double ret = Double.MAX_VALUE;
+            for(double i : inputs) {
+                ret = Math.min(ret, i);
+            }
+            return ret;
+        }
+
+        private double getMax(Vector<Double> inputs){
+            double ret = -1;
+            for(double i : inputs) {
+                ret = Math.max(ret, i);
+            }
+            return ret;
+        }
+
+        private double getAvg(Vector<Double> inputs){
+            if(inputs.isEmpty())
+                return 0;
+            double ret = 0;
+            for(double i : inputs) {
+                ret += i;
+            }
+            return ret / inputs.size();
+        }
+
+        public void run() {
+            try {
+                printHeader();
+                while(true){
+                    Thread.sleep(reportCycles*1000);
+                    print();
+                }
+            }
+            catch (Exception e) {
+
+            }
         }
     }
 }
